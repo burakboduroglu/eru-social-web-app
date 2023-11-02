7623a39690b47afa2534fd7abbb6cee4a3dc69ee
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2023-11-02 20:43:38 +0300

    fixed: footer responsiveness

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex flex-col justify-center items-center mt-20">
+    <div className="flex flex-col justify-center items-center min-h-screen">
       <SignIn />;
       <Footer />
     </div>
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex flex-col justify-center items-center mt-[10px]">
+    <div className="flex flex-col justify-center items-center min-h-screen">
       <SignUp />;
       <Footer />
     </div>
