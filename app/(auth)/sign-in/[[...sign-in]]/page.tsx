dbba8fb0213183618e0b9ea2a5b8cb79d9411386
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2023-11-05 20:02:39 +0300

    updated: UI design

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
-      <SignIn />;
+      <SignIn />
       <Footer />
     </div>
   );
