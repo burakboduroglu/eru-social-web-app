9dda6df216fe1c91b3a92153070cfcfdd75706d7
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2023-11-14 23:05:16 +0300

    updated vercel

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,6 +6,8 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { trTR } from "@clerk/localizations";
 
+import { Analytics } from "@vercel/analytics/react";
+
 export const metadata = {
   title: "Eru Social Web",
   description: "Erciyes University Social Web",
@@ -28,6 +30,7 @@ export default function RootLayout({
         <link rel="icon" href="../favicon.ico" sizes="any" />
         <body className={`${inter.className} bg-dark-1`}>{children}</body>
       </html>
+      <Analytics />
     </ClerkProvider>
   );
 }
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,52 +1,51 @@
 // CSS - Fonts
-import '../globals.css'
-import { Inter } from 'next/font/google'
+import "../globals.css";
+import { Inter } from "next/font/google";
 
 // Clerk
-import { ClerkProvider } from '@clerk/nextjs'
-import {trTR} from '@clerk/localizations';
+import { ClerkProvider } from "@clerk/nextjs";
+import { trTR } from "@clerk/localizations";
 
 // Components
-import Topbar from '@/components/shared/Topbar'
-import LeftSidebar from '@/components/shared/LeftSidebar'
-import RightSidebar from '@/components/shared/RightSidebar'
-import Bottombar from '@/components/shared/Bottombar'
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
 
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Eru Social Web',
-  description: 'Erciyes University Social Web',
+  title: "Eru Social Web",
+  description: "Erciyes University Social Web",
   icons: {
-    icon: '/assets/logo.svg',
+    icon: "/assets/logo.svg",
   },
-  creator: 'Burak Boduroglu',
-}
-
+  creator: "Burak Boduroglu",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <ClerkProvider localization={trTR}>
       <html lang="en">
         <body className={inter.className}>
           <Topbar />
-            <main className='flex flex-row'>
-              <LeftSidebar />
-              <section className='main-container'>
-                <div className='w-full max-w-6xl '>
-                  {children}
-                </div>
-              </section>
-              <RightSidebar />
-            </main>
+          <main className="flex flex-row">
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-6xl ">{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
           <Bottombar />
         </body>
       </html>
+      <Analytics />
     </ClerkProvider>
-  )
+  );
 }
