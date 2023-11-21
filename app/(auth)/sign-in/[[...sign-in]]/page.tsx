4190ec263352d9ef6e3e92f551b7051383fb52b6
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2023-11-21 13:02:16 +0300

    Sign In & Sign Up responsiveness

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -4,10 +4,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-gradient-custom">
-      <div className="mb-5">
-        <SignIn />
-      </div>
-      <Footer />
+      <SignIn />
     </div>
   );
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -4,10 +4,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-gradient-custom">
-      <div className="mb-5">
-        <SignUp />
-      </div>
-      <Footer />
+      <SignUp />
     </div>
   );
 }
