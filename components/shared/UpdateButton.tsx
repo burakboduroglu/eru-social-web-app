c580ef436b90d5471a41deae91729d0b0595cc0e
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2023-12-03 17:12:08 +0300

    Refactor UpdateButton component to use Link and add toast notification

--- components/shared/UpdateButton.tsx
+++ components/shared/UpdateButton.tsx
@@ -1,17 +1,22 @@
 "use client";
+import Link from "next/link";
 import React from "react";
+import "react-toastify/dist/ReactToastify.css";
+import { toast, ToastContainer } from "react-toastify";
 
 export default function UpdateButton() {
-  const handleClick = () => {
-    window.location.reload();
-  };
-
   return (
-    <button
-      onClick={handleClick}
-      className="text-[0.75em] text-white flex items-center justify-center border border-gray-600 p-3 hover:bg-gray-900 rounded-md"
-    >
-      <span className="font-bold text-blue">Güncelle</span>
-    </button>
+    <div>
+      <ToastContainer limit={3} theme={"dark"} autoClose={1500} />
+      <Link
+        href="/"
+        className="text-[0.75em] text-white flex items-center justify-center border border-gray-600 p-3 hover:bg-gray-900 rounded-md"
+        onClick={() => {
+          toast.info("Akış güncelleniyor");
+        }}
+      >
+        <span className="font-bold text-blue">Güncelle</span>
+      </Link>
+    </div>
   );
 }
