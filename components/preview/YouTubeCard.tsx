58847c56747f7f29c43e4014eaf03c9e4c06c7a4
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2023-12-27 22:55:58 +0300

    Add ReactPlayer component and update dependencies

--- components/preview/YouTubeCard.tsx
+++ components/preview/YouTubeCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactPlayer from "react-player/lazy";
 
 interface YoutubeEmbedProps {
   embedId: string;
@@ -6,13 +7,10 @@ interface YoutubeEmbedProps {
 
 const YoutubeEmbed = ({ embedId }: YoutubeEmbedProps) => (
   <div className="aspect-w-16 aspect-h-9">
-    <iframe
-      width="853"
-      height="480"
-      src={`https://www.youtube.com/embed/${embedId}`}
-      allowFullScreen
-      title="Embedded youtube"
-      className="rounded-lg w-full h-full"
+    <ReactPlayer
+      url={`https://www.youtube.com/embed/${embedId}`}
+      width={300}
+      height={200}
     />
   </div>
 );
